bf011dfaa68ba9180b3c4d7c8585e08683b8e534
Leila Ticiane Barbosa De Lima Dantas <lebarbos@c1r15s3.42porto.com>
2023-06-12 16:36:05 +0100

    new

--- getnextline4/get_next_line.c
+++ getnextline4/get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lebarbos <lebarbos@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/11 16:27:29 by lebarbos          #+#    #+#             */
-/*   Updated: 2023/06/12 13:16:51 by lebarbos         ###   ########.fr       */
+/*   Created: 2023/06/12 12:13:03 by lebarbos          #+#    #+#             */
+/*   Updated: 2023/06/12 16:33:47 by lebarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,52 @@
 
 static char	*read_and_stash(int fd, char *stash)
 {
-	char	*buffer;
-	int		bytes;
+	char		*buffer;
+	int			bytes;
 
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
-		return(NULL);
+		return (NULL);
 	bytes = 1;
-	while (!ft_strchr(stash, '\n') && bytes != 0)
+	while(!ft_strchr(stash, '\n') && bytes != 0)
 	{
-		bytes = read (fd, buffer, BUFFER_SIZE);
+		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes < 0)
 		{
-			free (stash);
-			free (buffer);
+			free(stash);
+			free(buffer);
 			return (NULL);
 		}
 		buffer[bytes] = '\0';
 		if (!buffer)
 			return (NULL);
-		stash = ft_strjoin (stash, buffer);
+		stash = ft_strjoin(stash, buffer);
 	}
 	free(buffer);
 	return (stash);
+}
+
+static char	*extract_line(char *stash)
+{
+	char	*line;
+	char	*next_line;
+	size_t	size_line;
+	size_t	i;
+
+	i = 0;
+	next_line = ft_strchr(stash, '\n');
+	size_line = ft_strlen(stash) - ft_strlen(next_line) + 2;
+	line = malloc(sizeof(char) * size_line);
+	if(!line)
+		return (NULL);
+	while (i < size_line - 1)
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	line[i] = '\0';
+	free(next_line);
+	return(line);
 }
 
 static char	*clean_stash(char *stash)
@@ -45,18 +68,17 @@ static char	*clean_stash(char *stash)
 	char	*next_line;
 	size_t	i;
 	size_t	j;
-
+	
 	next_line = ft_strchr(stash, '\n');
-	if(!next_line)
+	if (!next_line)
 	{
 		free(stash);
-		stash = NULL;
-		return (stash);
+		return (NULL);
 	}
 	new_stash = malloc(sizeof(char) * ft_strlen(next_line));
-	i = 0;
 	j = ft_strlen(stash) - ft_strlen(next_line) + 1;
-	while (i < ft_strlen(next_line))
+	i = 0;
+	while (i < j)
 	{
 		new_stash[i] = stash[j + i];
 		i++;
@@ -66,36 +88,16 @@ static char	*clean_stash(char *stash)
 	return (new_stash);
 }
 
-static char	*extract_line(char *stash)
-{
-	size_t	i;
-	char	*line;
-	char	*new_line;
-	size_t	total_size;
-
-	i = 0;
-	if (!stash)
-		return (NULL);
-	new_line = ft_strchr(stash, '\n');
-	total_size = ft_strlen(stash) - ft_strlen(new_line) + 1;
-	line = malloc(sizeof(char) * total_size);
-	while (i < total_size)
-	{
-		line[i] = stash[i];
-		i++;
-	}
-	free (new_line);
-	return (line);
-}
-
 char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char		*line;
-
+	
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
+		return (0);
 	stash = read_and_stash(fd, stash);
+	if(!stash)
+		return (NULL);
 	line = extract_line(stash);
 	stash = clean_stash(stash);
 	return (line);
@@ -106,7 +108,6 @@ char	*get_next_line(int fd)
 // 	int fd;
 // 	char	*str;
 	
-// 	str = (char *)-1;
 // 	fd = open("test.txt", O_RDONLY);
 // 	str = get_next_line(fd);
 // 	printf("Line: %s", str);
